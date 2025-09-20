2aac63bb13e914d6d3c9b55c024eb683df76a7d6
marioaje <marioaje@gmail.com>
2025-09-19 19:29:23 -0600

    Parte 1
    19 septiembre

---
+++ ProyectoFront/ProyectoFront/Controllers/CarroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoFront.Controllers
+{
+    public class CarroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
