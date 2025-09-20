6596eb88a22b4e9100fa4f4d3586abebfac31254
marioaje <marioaje@gmail.com>
2025-09-19 20:26:45 -0600

    Suma funcion con calculadora

--- ProyectoFront/ProyectoFront/Controllers/FuncionesController.cs
+++ ProyectoFront/ProyectoFront/Controllers/FuncionesController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+using ProyectoFront.Models;
 
 namespace ProyectoFront.Controllers
 {
     public class FuncionesController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(new Operacion());
+        }
+
+        [HttpPost]
+        public IActionResult Calculadora(Operacion operacion, string tipooperacion)
+        {
+
+            operacion.tipooperacion = "El resultado " + tipooperacion;
+
+            return View("Index", operacion);
+           
         }
     }
 }
---
+++ ProyectoFront/ProyectoFront/Models/Operacion.cs
@@ -0,0 +1,15 @@
+﻿namespace ProyectoFront.Models
+{
+    public class Operacion
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double datoa { get; set;  }
+        public double datob { get; set; }
+        public double resultado { get; set; }
+        public string? tipooperacion { get; set; }
+
+    }
+}
