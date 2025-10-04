866bc54e5e379b2760eb84e18cfed5d06eff70fb
marioaje <marioaje@gmail.com>
2025-10-03 20:20:55 -0600

    Editar y Eliminar

--- ProyectoFront/ProyectoFront/Controllers/ProductosController.cs
+++ ProyectoFront/ProyectoFront/Controllers/ProductosController.cs
@@ -50,5 +50,97 @@ namespace ProyectoFront.Controllers
 
             return View(_producto);
         }
+
+
+        //Get /Editar/Id
+
+        public async Task<IActionResult> Editar(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ProductoModel itemProducto = await _context.Producto.FindAsync(id);
+            if (itemProducto == null)
+            {
+                return NotFound();
+            }
+            return View(itemProducto);
+        }
+
+
+        //Post
+        [HttpPost]
+        public async Task<IActionResult> Editar(ProductoModel _producto)
+        {
+
+            //ProductoModel itemProducto = await _context.Producto.FindAsync(_producto.Id);
+            //if (itemProducto == null)
+            //{
+            //    return NotFound();
+            //}
+
+            if (ModelState.IsValid)
+            {
+
+                _context.Update(_producto);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+
+            return View(_producto);
+        }
+
+
+
+
+        //Get /Eliminar/Id
+
+        public async Task<IActionResult> Eliminar(int? id)
+        {
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+            ProductoModel itemProducto = await _context.Producto.FindAsync(id);
+            
+            if (itemProducto == null)
+            {
+                return NotFound();
+            }
+            return View(itemProducto);
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Eliminar(int Id)
+        {
+
+            if (Id == null)
+            {
+                return NotFound();
+            }
+            ProductoModel itemProducto = await _context.Producto.FindAsync(Id);
+
+            if (itemProducto == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _context.Producto.Remove(itemProducto);
+                await _context.SaveChangesAsync();
+            }
+          
+
+
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
